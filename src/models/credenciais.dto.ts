57fe3a19d74bdf2951e7842a9b08882379db1cdc
marcelojbcosta <marcelomogueco@gmail.com>
2021-11-01 21:24:31 -0300

    Obtendo dados do formulario de login

---
+++ src/models/credenciais.dto.ts
@@ -0,0 +1,6 @@
+
+
+export interface CredenciaisDTO{
+  email : string;
+  senha : string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 
 /*
@@ -11,6 +12,11 @@ controlador da pagina
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds : CredenciaisDTO = {
+    email : "",
+    senha : ""
+  };
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
@@ -26,9 +32,8 @@ depois que o login for feito o mnu lateral sera habilitado novamente
     this.menu.swipeEnable(true);
 }
 
-
-
   login(){
+    console.log(this.creds)
     this.navCtrl.setRoot('CategoriasPage');
   }
 
