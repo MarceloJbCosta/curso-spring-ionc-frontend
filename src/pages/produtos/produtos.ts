3a2cca0c7954960e5584cf92fadc8aae6e5e74e5
marcelojbcosta <marcelomogueco@gmail.com>
2022-01-04 21:05:33 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,7 +1,7 @@
 import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
@@ -15,19 +15,23 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController
     ) {
   }
 
   ionViewDidLoad() {
   let categoria_id = this.navParams.get('categoria_id')
-
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
     .subscribe(response =>{
       this.items = response['content'];
+      loader.dismiss();
       this.loadImageUrls();
     },
-    error =>{});
+    error =>{
+      loader.dismiss();
+    });
   }
 
   loadImageUrls(){
@@ -43,5 +47,16 @@ export class ProdutosPage {
   showDetail(produto_id: string){
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
+
+  //tela de lading
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde...",
+    });
+
+    loader.present();
+    return loader;
+  }
+
 
 }
